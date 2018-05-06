a55660d1e9b25182a13483964281599a70b8654b
Mikhail <mihail_kotov_86@mail.ru>
2018-05-06 17:31:39 +0300

    Спрайты

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,10 @@ var rename = require("gulp-rename");
 var imagemin = require("gulp-imagemin");
 var webp = require("gulp-webp");
 var svgstore = require("gulp-svgstore");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
 var server = require("browser-sync").create();
+var run = require("run-sequence");
 
 gulp.task("style", function() {
   gulp.src("source/less/style.less")
@@ -51,6 +54,14 @@ gulp.task("sprite", function () {
     .pipe(gulp.dest("source/img"));
 });
 
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("source"));
+});
+
 gulp.task("serve", ["style"], function() {
   server.init({
     server: "source/",
@@ -62,4 +73,8 @@ gulp.task("serve", ["style"], function() {
 
   gulp.watch("source/less/**/*.less", ["style"]);
   gulp.watch("source/*.html").on("change", server.reload);
+});
+
+gulp.task("build", function( done) {
+  run("style", "sprite", "html", done);
 });
