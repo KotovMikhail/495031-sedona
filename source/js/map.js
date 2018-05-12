ee4b87b7f787625275aec311a12f1f97b3d6e9da
Mikhail <mihail_kotov_86@mail.ru>
2018-05-12 19:01:33 +0300

    Правки

--- source/js/map.js
+++ source/js/map.js
@@ -5,12 +5,17 @@ myPlacemark;
 function init(){
     myMap = new ymaps.Map("map", {
         center: [34.87195183, -111.75626900],
-        zoom: 13
+        zoom: 7
     });
 
     myPlacemark = new ymaps.Placemark([34.87195183, -111.75626900], {
         hintContent: 'Седона Соединенные Штаты Америки, Аризона, Коконино-Каунти!',
         balloonContent: 'Добро пожаловать!'
+      }, {
+          iconLayout: "default#image",
+          iconImageHref: "../img/icon-map-marker.svg", // картинка иконки
+          iconImageSize: [27, 27], // размер иконки
+          iconImageOffset: [-10, -25] // позиция иконки
     });
 
     myMap.geoObjects.add(myPlacemark);
